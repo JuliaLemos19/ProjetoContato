9ce9d172adbf799d186c49437bd66b6a05d2d043
Julia Lemos <77756159+JuliaLemos19@users.noreply.github.com>
2024-10-01 20:41:04 -0300

    Add files via upload

---
+++ projetoContato/Contato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoContato
+{
+    internal class Contato
+    {
+
+        public string Email { get => Email; set => Email = value; }
+        public string Nome { get => Nome; set => Nome = value; }
+        public Data dtNasc { get => dtNasc; set => dtNasc = value; }
+
+        List<Telefone> Telefones { get => Telefones; set => Telefones = value; }
+
+
+
+        public int getIdade(Data dtNasc, int Data)
+        {
+
+        }
+
+    }
+}
+           
+
+
+    
+
